c7e9cfac0f2ad597068e620cefaa0a5cc7316739
Nawarian <nickolas@phpsp.org.br>
2023-01-18 16:35:39 +0100

    test(Solver) add tests to /api/v1/solve endpoint

--- test/handlers/solver.test.ts
+++ test/handlers/solver.test.ts
@@ -21,3 +21,39 @@ test("POST /api/v1/solve returns 200", async () => {
 		expect(await res.text()).toBe(output);
 	});
 });
+
+test("POST /api/v1/solve returns 400", async () => {
+	const invalidLengthSudokuString = '0'.repeat(80);
+
+	const res = await handleRequest(
+		new Request(`http://sudoku.codamos.com.br/api/v1/solve?input=${invalidLengthSudokuString}`, {
+				method: "POST",
+			}),
+	);
+
+	expect(res.status).toBe(400);
+	expect(await res.text()).toBe('Invalid sudoku string: provide a 81-length digits stream.');
+
+	const invalidCharactersSudokuString = 'a'.repeat(81);
+	const res2 = await handleRequest(
+		new Request(`http://sudoku.codamos.com.br/api/v1/solve?input=${invalidCharactersSudokuString}`, {
+				method: "POST",
+			}),
+	);
+
+	expect(res2.status).toBe(400);
+	expect(await res2.text()).toBe('Invalid sudoku string: all characters must be a 0 to 9 digit.');
+});
+
+test("POST /api/v1/solve returns 500", async () => {
+	const impossibleSudoku = '0'.repeat(81);
+
+	const res = await handleRequest(
+		new Request(`http://sudoku.codamos.com.br/api/v1/solve?input=${impossibleSudoku}`, {
+				method: "POST",
+			}),
+	);
+
+	expect(res.status).toBe(500);
+	expect(await res.text()).toBe('Could not solve sudoku game.');
+});
